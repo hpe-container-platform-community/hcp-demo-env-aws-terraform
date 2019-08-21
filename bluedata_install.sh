bbabc80f7f77d291937944bceed118566cb826c0
chris snow <chsnow123@gmail.com>
2019-08-21 10:06:09 +0100

    add install controller step (broken)

--- bluedata_install.sh
+++ bluedata_install.sh
@@ -85,15 +85,15 @@ ENDSSH
 
 # We have password SSH access from our local machines to EC2, so we can utiise this to copy the Controller SSH key to each Worker
 for WRKR in ${WRKR_PUB_IPS[@]}; do 
-ssh -o StrictHostKeyChecking=no -i ${LOCAL_SSH_PRV_KEY_PATH} -T centos@${CTRL_PUB_IP} "cat /home/centos/.ssh/id_rsa.pub" | \
-  ssh -o StrictHostKeyChecking=no -i ${LOCAL_SSH_PRV_KEY_PATH} -T centos@${WRKR} "cat >> /home/centos/.ssh/authorized_keys"
+    ssh -o StrictHostKeyChecking=no -i ${LOCAL_SSH_PRV_KEY_PATH} -T centos@${CTRL_PUB_IP} "cat /home/centos/.ssh/id_rsa.pub" | \
+        ssh -o StrictHostKeyChecking=no -i ${LOCAL_SSH_PRV_KEY_PATH} -T centos@${WRKR} "cat >> /home/centos/.ssh/authorized_keys"
 done
 
 # test passwordless SSH connection from Controller to Workers
 for WRKR in ${WRKR_PRV_IPS[@]}; do 
-ssh -o StrictHostKeyChecking=no -i ${LOCAL_SSH_PRV_KEY_PATH} -T centos@${CTRL_PUB_IP} << ENDSSH
-echo CONTROLLER: Connecting to WORKER ${WRKR}...
-ssh -o ConnectTimeout=10 -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa -T centos@${WRKR} "echo Connected!"
+    ssh -o StrictHostKeyChecking=no -i ${LOCAL_SSH_PRV_KEY_PATH} -T centos@${CTRL_PUB_IP} << ENDSSH
+        echo CONTROLLER: Connecting to WORKER ${WRKR}...
+        ssh -o ConnectTimeout=10 -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa -T centos@${WRKR} "echo Connected!"
 ENDSSH
 done
 
@@ -112,10 +112,6 @@ ssh -o StrictHostKeyChecking=no -i ${LOCAL_SSH_PRV_KEY_PATH} -T centos@${GATW_PU
    (sudo reboot)&
 ENDSSH
 
-echo 'Waiting for Gateway to reboot '
-while ! nc -w5 -z ${GATW_PUB_IP} 22; do printf "." -n ; done;
-echo 'Gateway has reboot'
-
 #
 # Controller
 #
@@ -127,25 +123,35 @@ ssh -o StrictHostKeyChecking=no -i ${LOCAL_SSH_PRV_KEY_PATH} -T centos@${CTRL_PU
    (sudo reboot)&
 ENDSSH
 
-echo 'Waiting for Controller to reboot '
-while ! nc -w5 -z ${CTRL_PUB_IP} 22; do printf "." -n ; done;
-echo 'Controller has reboot'
-
 #
 # Workers
 #
 
 for WRKR in ${WRKR_PUB_IPS[@]}; do 
-ssh -o StrictHostKeyChecking=no -i ${LOCAL_SSH_PRV_KEY_PATH} -T centos@${WRKR} << ENDSSH
-   curl -s -f ${EPIC_RPM_DL_URL} | grep wrkr | awk '{print \$3}' | sed -r "s/([a-zA-Z0-9_+]*)(-[a-zA-Z0-9]+)?(-\S+)(-.*)/\1\2\3/" | xargs sudo yum install -y 
+   ssh -o StrictHostKeyChecking=no -i ${LOCAL_SSH_PRV_KEY_PATH} -T centos@${WRKR} << ENDSSH
+    curl -s -f ${EPIC_RPM_DL_URL} | grep wrkr | awk '{print \$3}' | sed -r "s/([a-zA-Z0-9_+]*)(-[a-zA-Z0-9]+)?(-\S+)(-.*)/\1\2\3/" | xargs sudo yum install -y 
 
-   # wrap reboot to prevent ssh thinking the session aborted due to an error
-   (sudo reboot)&
+    # wrap reboot to prevent ssh thinking the session aborted due to an error
+    (sudo reboot)&
 ENDSSH
+done
 
-echo "Waiting for Worker ${WRKR} to reboot "
-while ! nc -w5 -z ${WRKR} 22; do printf "." -n ; done;
-echo 'Worker has reboot'
+#
+# Wait for Gateway, Controller and Workers to come online after reboot
+#
+
+echo 'Waiting for Gateway ssh session '
+while ! nc -w5 -z ${GATW_PUB_IP} 22; do printf "." -n ; done;
+echo 'Gateway has rebooted'
+
+echo 'Waiting for Controller ssh session '
+while ! nc -w5 -z ${CTRL_PUB_IP} 22; do printf "." -n ; done;
+echo 'Controller has rebooted'
+
+for WRKR in ${WRKR_PUB_IPS[@]}; do 
+    echo "Waiting for Worker ${WRKR} ssh session"
+    while ! nc -w5 -z ${WRKR} 22; do printf "." -n ; done;
+    echo 'Worker has rebooted'
 done
 
 ###############################################################################
@@ -180,32 +186,32 @@ IGNORE_WRKR_PRECHECK_FAIL=true
 
 for INDEX in ${!WRKR_PUB_IPS[@]}; do 
 
-WRKR_PUB_IP=${WRKR_PUB_IPS[$INDEX]}
-WRKR_PRV_IP=${WRKR_PRV_IPS[$INDEX]}
+   WRKR_PUB_IP=${WRKR_PUB_IPS[$INDEX]}
+   WRKR_PRV_IP=${WRKR_PRV_IPS[$INDEX]}
 
-ssh -o StrictHostKeyChecking=no -i ${LOCAL_SSH_PRV_KEY_PATH} -T centos@${WRKR_PUB_IP} << ENDSSH
+   ssh -o StrictHostKeyChecking=no -i ${LOCAL_SSH_PRV_KEY_PATH} -T centos@${WRKR_PUB_IP} << ENDSSH
  
-   # ensure drives are mounted before pre-checking
-   while ! mountpoint -x /dev/xvdb; do sleep 1; done
-   while ! mountpoint -x /dev/xvdc; do sleep 1; done
+    # ensure drives are mounted before pre-checking
+    while ! mountpoint -x /dev/xvdb; do sleep 1; done
+    while ! mountpoint -x /dev/xvdc; do sleep 1; done
 
-   curl -s -o bluedata-prechecks-epic-entdoc-3.7.bin ${EPIC_PRECHECK_DL_URL}
-   chmod +x bluedata-prechecks-epic-entdoc-3.7.bin
+    curl -s -o bluedata-prechecks-epic-entdoc-3.7.bin ${EPIC_PRECHECK_DL_URL}
+    chmod +x bluedata-prechecks-epic-entdoc-3.7.bin
 
-   if [ "$IGNORE_WRKR_PRECHECK_FAIL" == 'true' ];
-   then
-      sudo ./bluedata-prechecks-epic-entdoc-3.7.bin -w \
-        --worker-primary-ip ${WRKR_PRV_IP} \
-        --controller-ip ${CTRL_PRV_IP} \
-        --gateway-node-ip ${GATW_PRV_IP} \
-        --gateway-node-hostname ${GATW_PRV_DNS} || true 2>&1 > /home/centos/bluedata-precheck.log
-   else
-      sudo ./bluedata-prechecks-epic-entdoc-3.7.bin -w \
-        --worker-primary-ip ${WRKR_PRV_IP} \
-        --controller-ip ${CTRL_PRV_IP} \
-        --gateway-node-ip ${GATW_PRV_IP} \
-        --gateway-node-hostname ${GATW_PRV_DNS} || exit 1 2>&1 > /home/centos/bluedata-precheck.log
-   fi
+    if [ "$IGNORE_WRKR_PRECHECK_FAIL" == 'true' ];
+    then
+        sudo ./bluedata-prechecks-epic-entdoc-3.7.bin -w \
+            --worker-primary-ip ${WRKR_PRV_IP} \
+            --controller-ip ${CTRL_PRV_IP} \
+            --gateway-node-ip ${GATW_PRV_IP} \
+            --gateway-node-hostname ${GATW_PRV_DNS} || true 2>&1 > /home/centos/bluedata-precheck.log
+    else
+        sudo ./bluedata-prechecks-epic-entdoc-3.7.bin -w \
+            --worker-primary-ip ${WRKR_PRV_IP} \
+            --controller-ip ${CTRL_PRV_IP} \
+            --gateway-node-ip ${GATW_PRV_IP} \
+            --gateway-node-hostname ${GATW_PRV_DNS} || exit 1 2>&1 > /home/centos/bluedata-precheck.log
+    fi
 ENDSSH
 done
 
@@ -213,42 +219,53 @@ done
 # Install Controller
 ###############################################################################
 
-    #   "curl -s -o bluedata-epic-entdoc-minimal-release-3.7-2207.bin ${var.epic_dl_url}",
-    #   "chmod +x bluedata-epic-entdoc-minimal-release-3.7-2207.bin",
+scp -o StrictHostKeyChecking=no -i ${LOCAL_SSH_PRV_KEY_PATH} initial_bluedata_config.py centos@${CTRL_PUB_IP}:/home/centos/initial_bluedata_config.py
 
-    #   # install EPIC 
-    #   "sudo ./bluedata-epic-entdoc-minimal-release-3.7-2207.bin -s -i -c ${aws_instance.controller.private_ip} --user centos --group centos",
-      
-    #   # install application workbench
-    #   "sudo yum install -y python-pip",
-    #   "sudo pip install --upgrade pip",
-    #   "sudo pip install --upgrade setuptools",
-    #   "sudo pip install --upgrade bdworkbench",
+ssh -o StrictHostKeyChecking=no -i ${LOCAL_SSH_PRV_KEY_PATH} -T centos@${CTRL_PUB_IP} << ENDSSH
+   curl -s -o bluedata-epic-entdoc-minimal-release-3.7-2207.bin ${EPIC_DL_URL}
+   chmod +x bluedata-epic-entdoc-minimal-release-3.7-2207.bin
 
-    #   # initial configuration
-    #   "sudo pip install beautifulsoup4",
-    #   "sudo pip install lxml",
-    #   "python /home/centos/initial_bluedata_config.py"
+   # install EPIC
+   sudo ./bluedata-epic-entdoc-minimal-release-3.7-2207.bin -s -i -c ${CTRL_PRV_IP} --user centos --group centos
 
-#  1. At the login screen, use 'admin/admin123'
-#  2. Navigate to Installation tab
+   # install application workbench
+   sudo yum install -y epel-release
+   sudo yum install -y python-pip
+   sudo pip install --upgrade pip
+   sudo pip install --upgrade setuptools
+   sudo pip install --upgrade bdworkbench
 
-# Then ...
+   # initial configuration
+   sudo pip install beautifulsoup4
+   sudo pip install lxml
 
-#  1. run `terraform output` to see all variables
-#  1. Add workers private ip 
-#  2. Add gateway private ip and private dns
-#  3. Download contoller ssh key (see output variable retrive_controller_ssh for command to run locally) 
-#     This will be something like: `ssh -o StrictHostKeyChecking=no -i /home/snowch/.ssh/id_rsa centos@18.130.217.53 'cat ~/.ssh/id_rsa' > controller.prv_key`
-#  4. Upload controller.prv_key
-#  5. Click Add hosts (enter site lock down when prompted)
+   # Accept the defaults on the first configuration screen after installing BlueData
+   python /home/centos/initial_bluedata_config.py
+ENDSSH
 
-# After a few minutes, you should see Gateway 'Installed' and Workers 'Bundle completed'
+echo "** BlueData installation completed successfully.  You now need to configure it **"
+echo "Controller URL: http://${CTRL_PUB_IP - login: admin/admin123"
+echo "Worker IPs: ${WRKR_PRV_IP[@]}"
+echo "Gateway IP: ${GATW_PRV_IP}"   # should this be the public IP?
+echo "Gateway DNS: ${GATW_PRV_DNS}" # should this be the public DNS?
 
-#  1. Select each Worker
-#  2. Click 'Install'
-#  3. Wait a few minutes
+echo "Downloading Controller SSH Private key to 'controller.prv_key' ** PLEASE KEEP IT SECURE **"
+ssh -o StrictHostKeyChecking=no -i ${LOCAL_SSH_PRV_KEY_PATH} centos@${CTRL_PUB_IP} 'cat ~/.ssh/id_rsa' > controller.prv_key
 
+cat << EOF
+Instructions:
 
+1. At the login screen, use 'admin/admin123'
+2. Navigate to Installation tab
+   1. Add workers private ip ${WRKR_PRV_IP[@]} 
+   2. Add gateway private ip and private dns ${GATW_PRV_IP} | ${GATW_PRV_DNS}
+   3. Upload controller.prv_key
+   4. Click Add hosts (enter site lock down when prompted)
 
-echo 'Done!'
+   # After a few minutes, you should see Gateway 'Installed' and Workers 'Bundle completed'
+
+   5. Select each Worker
+   6. Click 'Install'
+   7. Wait a few minutes
+EOF
+
