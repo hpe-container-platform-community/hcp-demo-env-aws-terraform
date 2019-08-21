2cc36421d2f0838f5fe88978eddbf616835f90e8
chris snow <chsnow123@gmail.com>
2019-08-21 01:55:24 +0100

    add controller precheck

--- bluedata_install.sh
+++ bluedata_install.sh
@@ -13,6 +13,7 @@ LOCAL_SSH_PRV_KEY_PATH=$(cat output.json | python3 -c 'import json,sys;obj=json.
 CLIENT_CIDR_BLOCK=$(cat output.json | python3 -c 'import json,sys;obj=json.load(sys.stdin);print (obj["client_cidr_block"]["value"])') 
 
 EPIC_RPM_DL_URL=$(cat output.json | python3 -c 'import json,sys;obj=json.load(sys.stdin);print (obj["epic_rpm_dl_url"]["value"])') 
+EPIC_PRECHECK_DL_URL=$(cat output.json | python3 -c 'import json,sys;obj=json.load(sys.stdin);print (obj["epic_precheck_dl_url"]["value"])') 
 
 CTRL_PRV_IP=$(cat output.json | python3 -c 'import json,sys;obj=json.load(sys.stdin);print (obj["controller_private_ip"]["value"])') 
 CTRL_PUB_IP=$(cat output.json | python3 -c 'import json,sys;obj=json.load(sys.stdin);print (obj["controller_public_ip"]["value"])') 
@@ -22,9 +23,11 @@ echo CTRL_PUB_IP=$CTRL_PUB_IP
 
 GATW_PRV_IP=$(cat output.json | python3 -c 'import json,sys;obj=json.load(sys.stdin);print (obj["gateway_private_ip"]["value"])') 
 GATW_PUB_IP=$(cat output.json | python3 -c 'import json,sys;obj=json.load(sys.stdin);print (obj["gateway_public_ip"]["value"])') 
+GATW_PRV_DNS=$(cat output.json | python3 -c 'import json,sys;obj=json.load(sys.stdin);print (obj["gateway_private_dns"]["value"])') 
 
 echo GATW_PRV_IP=$GATW_PRV_IP
 echo GATW_PUB_IP=$GATW_PUB_IP
+echo GATW_PRV_DNS=$GATW_PRV_DNS
 
 WRKR_PRV_IPS=$(cat output.json | python3 -c 'import json,sys;obj=json.load(sys.stdin);print (*obj["workers_private_ip"]["value"][0], sep=" ")') 
 WRKR_PUB_IPS=$(cat output.json | python3 -c 'import json,sys;obj=json.load(sys.stdin);print (*obj["workers_public_ip"]["value"][0], sep=" ")') 
@@ -83,53 +86,77 @@ done
 # Install RPMS
 ###############################################################################
 
-#
-# Gateway
-#
+# #
+# # Gateway
+# #
 
-ssh -o StrictHostKeyChecking=no -i ${LOCAL_SSH_PRV_KEY_PATH} -T centos@${GATW_PUB_IP} << ENDSSH
-   curl -s -f ${EPIC_RPM_DL_URL} | grep proxy | awk '{print \$3}' | sed -r "s/([a-zA-Z0-9_+]*)(-[a-zA-Z0-9]+)?(-\S+)(-.*)/\1\2\3/" | xargs sudo yum install -y 
+# ssh -o StrictHostKeyChecking=no -i ${LOCAL_SSH_PRV_KEY_PATH} -T centos@${GATW_PUB_IP} << ENDSSH
+#    curl -s -f ${EPIC_RPM_DL_URL} | grep proxy | awk '{print \$3}' | sed -r "s/([a-zA-Z0-9_+]*)(-[a-zA-Z0-9]+)?(-\S+)(-.*)/\1\2\3/" | xargs sudo yum install -y 
 
-   # wrap reboot to prevent ssh thinking the session aborted due to an error
-   (sudo reboot)&
-ENDSSH
+#    # wrap reboot to prevent ssh thinking the session aborted due to an error
+#    (sudo reboot)&
+# ENDSSH
 
-echo 'Waiting for Gateway to restart '
-while ! nc -w5 -z ${GATW_PUB_IP} 22; do printf "." -n ; done;
-echo 'Gateway has restarted'
+# echo 'Waiting for Gateway to restart '
+# while ! nc -w5 -z ${GATW_PUB_IP} 22; do printf "." -n ; done;
+# echo 'Gateway has restarted'
 
-#
-# Controller
-#
+# #
+# # Controller
+# #
 
-ssh -o StrictHostKeyChecking=no -i ${LOCAL_SSH_PRV_KEY_PATH} -T centos@${CTRL_PUB_IP} << ENDSSH
-   curl -s -f ${EPIC_RPM_DL_URL} | grep ctrl | awk '{print \$3}' | sed -r "s/([a-zA-Z0-9_+]*)(-[a-zA-Z0-9]+)?(-\S+)(-.*)/\1\2\3/" | xargs sudo yum install -y 
+# ssh -o StrictHostKeyChecking=no -i ${LOCAL_SSH_PRV_KEY_PATH} -T centos@${CTRL_PUB_IP} << ENDSSH
+#    curl -s -f ${EPIC_RPM_DL_URL} | grep ctrl | awk '{print \$3}' | sed -r "s/([a-zA-Z0-9_+]*)(-[a-zA-Z0-9]+)?(-\S+)(-.*)/\1\2\3/" | xargs sudo yum install -y 
 
-   # wrap reboot to prevent ssh thinking the session aborted due to an error
-   (sudo reboot)&
-ENDSSH
+#    # wrap reboot to prevent ssh thinking the session aborted due to an error
+#    (sudo reboot)&
+# ENDSSH
 
-echo 'Waiting for Controller to restart '
-while ! nc -w5 -z ${CTRL_PUB_IP} 22; do printf "." -n ; done;
-echo 'Controller has restarted'
+# echo 'Waiting for Controller to restart '
+# while ! nc -w5 -z ${CTRL_PUB_IP} 22; do printf "." -n ; done;
+# echo 'Controller has restarted'
 
-#
-# Workers
-#
+# #
+# # Workers
+# #
 
-for WRKR in ${WRKR_PUB_IPS[@]}; do 
-ssh -o StrictHostKeyChecking=no -i ${LOCAL_SSH_PRV_KEY_PATH} -T centos@${WRKR} << ENDSSH
-   curl -s -f ${EPIC_RPM_DL_URL} | grep wrkr | awk '{print \$3}' | sed -r "s/([a-zA-Z0-9_+]*)(-[a-zA-Z0-9]+)?(-\S+)(-.*)/\1\2\3/" | xargs sudo yum install -y 
+# for WRKR in ${WRKR_PUB_IPS[@]}; do 
+# ssh -o StrictHostKeyChecking=no -i ${LOCAL_SSH_PRV_KEY_PATH} -T centos@${WRKR} << ENDSSH
+#    curl -s -f ${EPIC_RPM_DL_URL} | grep wrkr | awk '{print \$3}' | sed -r "s/([a-zA-Z0-9_+]*)(-[a-zA-Z0-9]+)?(-\S+)(-.*)/\1\2\3/" | xargs sudo yum install -y 
 
-   # wrap reboot to prevent ssh thinking the session aborted due to an error
-   (sudo reboot)&
-ENDSSH
+#    # wrap reboot to prevent ssh thinking the session aborted due to an error
+#    (sudo reboot)&
+# ENDSSH
 
-echo "Waiting for Worker ${WRKR} to restart "
-while ! nc -w5 -z ${WRKR} 22; do printf "." -n ; done;
-echo 'Worker has restarted'
-done
+# echo "Waiting for Worker ${WRKR} to restart "
+# while ! nc -w5 -z ${WRKR} 22; do printf "." -n ; done;
+# echo 'Worker has restarted'
+# done
 
 ###############################################################################
 # Prechecks
 ###############################################################################
+
+# Controller
+
+IGNORE_CTRL_PRECHECK_FAIL=true
+
+ssh -o StrictHostKeyChecking=no -i ${LOCAL_SSH_PRV_KEY_PATH} -T centos@${CTRL_PUB_IP} << ENDSSH
+   curl -s -o bluedata-prechecks-epic-entdoc-3.7.bin ${EPIC_PRECHECK_DL_URL}
+   chmod +x bluedata-prechecks-epic-entdoc-3.7.bin
+
+   if [ "$IGNORE_CTRL_PRECHECK_FAIL" == 'true' ];
+   then
+      sudo ./bluedata-prechecks-epic-entdoc-3.7.bin -c \
+        --controller-ip ${CTRL_PRV_IP} \
+        --gateway-node-ip ${GATW_PRV_IP} \
+        --gateway-node-hostname ${GATW_PRV_DNS} || true 2>&1 > /home/centos/bluedata-precheck.log
+   else
+      sudo ./bluedata-prechecks-epic-entdoc-3.7.bin -c \
+        --controller-ip ${CTRL_PRV_IP} \
+        --gateway-node-ip ${GATW_PRV_IP} \
+        --gateway-node-hostname ${GATW_PRV_DNS} || exit 1 2>&1 > /home/centos/bluedata-precheck.log
+   fi
+ENDSSH
+
+echo 'Done!'
