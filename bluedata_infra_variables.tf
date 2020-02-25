1e4bd099c7c140fc45af40e5a5429156561cfa43
chris snow <chsnow123@gmail.com>
2020-02-25 08:41:44 +0000

    add more AMIs

--- bluedata_infra_variables.tf
+++ bluedata_infra_variables.tf
@@ -8,7 +8,7 @@ variable "check_client_ip" { default = "true" }
 variable "vpc_cidr_block" { }
 variable "subnet_cidr_block" { }
 
-// TODO - this is currently unused
+// TODO - EC2_CENTOS7_AMIS is currently unused
 variable "EC2_CENTOS7_AMIS" {
   # Find more https://console.aws.amazon.com/ec2/home?region=us-east-1#Images:visibility=public-images;search=aws-marketplace/CentOS%20Linux%207%20x86_64%20HVM%20EBS%20ENA%201805_01-b7ee8a69-ee97-4a49-9e68-afaee216db2e-ami-77ec9308.4
   default = { 
@@ -17,17 +17,14 @@ variable "EC2_CENTOS7_AMIS" {
     us-west-1      = "ami-4826c22b" # N.California
     us-west-2      = "ami-3ecc8f46" # Oregon
     ap-southeast-1 = "ami-8e0205f2" # Singapore
-    # TODO complete this ...
-    /* "ami-00846a67" # this is valid for the London region, change if required
-                                    #
-                                    # Frankfurt  : ami-dd3c0f36 ?
-                                    # Ireland    : ami-3548444c
-                                    # London     : ami-00846a67
-                                    # Paris      : ami-262e9f5b
-                                    # Stockholm  : ami-b133bccf ?
-                                    */
+    eu-central-1   = "ami-dd3c0f36" # Frankfurt
+    eu-west-1      = "ami-3548444c" # Ireland 
+    eu-west-2      = "ami-00846a67" # London
+    eu-west-3      = "ami-262e9f5b" # Paris
+    eu-north-1     = "ami-b133bccf" # Stockholm
   } 
 } 
+// USAGE:
 // var.ec2_ami = var.EC2_CENTOS7_AMIS[var.region]
 
 variable "ec2_ami" { }
