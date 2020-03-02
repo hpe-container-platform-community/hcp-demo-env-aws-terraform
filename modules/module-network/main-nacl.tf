daf8348f9d1f2f340126c5b680d10abb9d665871
chris snow <chsnow123@gmail.com>
2020-03-02 20:55:07 +0000

    fix problem with nacl switching

--- modules/module-network/main-nacl.tf
+++ modules/module-network/main-nacl.tf
@@ -1,7 +1,8 @@
-# Don't do anything with the default network acl except add tags
+
+// Don't do anything with the default network acl except add tags
+// to identify it with this project
 resource "aws_default_network_acl" "default" {
   default_network_acl_id = aws_vpc.main.default_network_acl_id
-  subnet_ids = [ aws_subnet.main.id ]
 
   tags = {
     Name = "${var.project_id}-default-network-acl"
@@ -21,20 +22,29 @@ resource "aws_network_acl" "main" {
   }
 }
 
-locals {
-  full_access_ips = concat([var.client_cidr_block], var.additional_client_ip_list)
+resource "aws_network_acl_rule" "allow_all_from_client_ips" {
+  # allow client machine to have full access to all hosts
+  network_acl_id = aws_network_acl.main.id
+
+  rule_number = 100
+  egress      = false
+  protocol    = "-1"
+  rule_action = "allow"
+  cidr_block  = var.client_cidr_block
+  from_port   = 0
+  to_port     = 0
 }
 
 resource "aws_network_acl_rule" "allow_all_from_specific_ips" {
   # allow specified machines to have full access to all hosts
   network_acl_id = aws_network_acl.main.id
 
-  count       = length(local.full_access_ips)
-  rule_number = 100 + count.index
+  count       = length(var.additional_client_ip_list)
+  rule_number = 110 + count.index
   egress      = false
   protocol    = "-1"
   rule_action = "allow"
-  cidr_block  = element(local.full_access_ips, count.index)
+  cidr_block  = element(var.additional_client_ip_list, count.index)
   from_port   = 0
   to_port     = 0
 }
