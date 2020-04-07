33b505c68507510de7a9dc32d1b36d8675837c62
chris snow <chsnow123@gmail.com>
2020-04-07 07:48:09 +0100

    only create if state is empty

--- bin/create_new_environment_from_scratch.sh
+++ bin/create_new_environment_from_scratch.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+if [[ -f terraform.tfstate ]];
+then
+   echo "********************************************************************************************************"
+   echo "Refusing to create environment because existing ./terraform.tfstate file found."
+   echo "Please destroy your environment (./bin/terraform_destroy.sh) and then remove all terraform.tfstate files"
+   echo "before trying again."
+   echo "********************************************************************************************************"
+   exit 1
+fi
+
 terraform apply -var-file=etc/bluedata_infra.tfvars -var="client_cidr_block=$(curl -s http://ifconfig.me/ip)/32" -auto-approve=true && \
 sleep 60 && \
 terraform output -json > generated/output.json && \
