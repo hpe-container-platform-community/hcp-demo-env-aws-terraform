b34ea11229d81eb6b715f60b9ab957bda6df8784
chris snow <chsnow123@gmail.com>
2020-04-13 11:12:42 +0100

    fix bugs

--- scripts/end_user_scripts/mapr_ldap/3_setup_datatap.sh
+++ scripts/end_user_scripts/mapr_ldap/3_setup_datatap.sh
@@ -9,6 +9,9 @@ source "$SCRIPT_DIR/../../variables.sh"
 
 ssh -o StrictHostKeyChecking=no -i "${LOCAL_SSH_PRV_KEY_PATH}" -T centos@${CTRL_PUB_IP} <<-SSH_EOF
 	set -xeu
+
+	# delete any previous files - start with a clean slate
+	rm -f create_dataconn.py settings.py session.py
 	
 	wget -c https://bluedata-releases.s3.amazonaws.com/dtap-mapr/create_dataconn.py
 	wget -c https://bluedata-releases.s3.amazonaws.com/dtap-mapr/settings.py
@@ -24,7 +27,7 @@ ssh -o StrictHostKeyChecking=no -i "${LOCAL_SSH_PRV_KEY_PATH}" -T centos@${CTRL_
 	sed -i 's#(url, data=data, headers=headers, timeout=20)#(url, data=data, headers=headers, timeout=20, verify="/home/centos/minica.pem")#g' session.py
 
 	chmod +x create_dataconn.py
-	./create_dataconn.py -n MaprClus1 -p /mapr/hcp.mapr.cluster/ -t file
+	./create_dataconn.py -n MaprClus1 -p /mapr/hcp.mapr.cluster/ -t file || echo "Unexpected Error"
 SSH_EOF
 
 for HOST in $CTRL_PUB_IP ${WRKR_PUB_IPS[@]}; 
@@ -33,41 +36,45 @@ do
 	ssh -o StrictHostKeyChecking=no -i "${LOCAL_SSH_PRV_KEY_PATH}" centos@$HOST <<-SSH_EOF
 		set -xeu
 
-		sudo bash -c "cat > /etc/yum.repos.d/maprtech.repo <<-EOF
-		[maprtech]
-		name=MapR Technologies
-		baseurl=http://package.mapr.com/releases/v5.1.0/redhat/
-		enabled=1
-		gpgcheck=0
-		protect=1
+		# if the host doesn't have bdconfig, it hasn't been added to HCP yet
+		if command -v bdconfig >/dev/null 2>&1; then
 
-		[maprecosystem]
-		name=MapR Technologies
-		baseurl=http://package.mapr.com/releases/ecosystem-5.x/redhat
-		enabled=1
-		gpgcheck=0
-		protect=1
-		EOF"
+			sudo bash -c "cat > /etc/yum.repos.d/maprtech.repo <<-EOF
+			[maprtech]
+			name=MapR Technologies
+			baseurl=http://package.mapr.com/releases/v5.1.0/redhat/
+			enabled=1
+			gpgcheck=0
+			protect=1
 
-		#wget http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
-		#sudo rpm -Uvh epel-release-6*.rpm
+			[maprecosystem]
+			name=MapR Technologies
+			baseurl=http://package.mapr.com/releases/ecosystem-5.x/redhat
+			enabled=1
+			gpgcheck=0
+			protect=1
+			EOF"
 
-		sudo rpm --import http://package.mapr.com/releases/pub/maprgpg.key
-		sudo yum install -y mapr-client.x86_64 java-1.7.0-openjdk.x86_64
-		[[ -d /mapr ]] || sudo mkdir /mapr
-		export LD_LIBRARY_PATH=/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.131.x86_64/jre/lib/amd64/server/:/opt/mapr/lib
-		docker cp epic-mapr:/opt/mapr/conf/ssl_truststore .
-		sudo cp /home/centos/ssl_truststore  /opt/mapr/conf/
-		sudo chown root:root /opt/mapr/conf/ssl_truststore
+			#wget http://download.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
+			#sudo rpm -Uvh epel-release-6*.rpm
 
-		sudo /opt/mapr/server/configure.sh -N hcp.mapr.cluster -c -secure -C $CTRL_PRV_IP -HS 
-		echo pass123 | maprlogin password -cluster hcp.mapr.cluster -user ad_admin1
-		maprlogin generateticket -type service -out /tmp/longlived_ticket -duration 30:0:0 -renewal 90:0:0
+			sudo rpm --import http://package.mapr.com/releases/pub/maprgpg.key
+			sudo yum install -y mapr-client.x86_64 java-1.7.0-openjdk.x86_64
+			[[ -d /mapr ]] || sudo mkdir /mapr
+			export LD_LIBRARY_PATH=/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.131.x86_64/jre/lib/amd64/server/:/opt/mapr/lib
+			docker cp epic-mapr:/opt/mapr/conf/ssl_truststore .
+			sudo cp /home/centos/ssl_truststore  /opt/mapr/conf/
+			sudo chown root:root /opt/mapr/conf/ssl_truststore
 
-		sudo yum install -y mapr-posix-client-*
-		sudo service mapr-posix-client-basic start
+			sudo /opt/mapr/server/configure.sh -N hcp.mapr.cluster -c -secure -C $CTRL_PRV_IP -HS 
+			echo pass123 | maprlogin password -cluster hcp.mapr.cluster -user ad_admin1
+			maprlogin generateticket -user ad_admin1 -type service -out /tmp/longlived_ticket -duration 30:0:0 -renewal 90:0:0
 
-		ls /mapr/
+			sudo yum install -y mapr-posix-client-*
+			sudo service mapr-posix-client-basic start
+
+			ls /mapr/
+		fi
 	SSH_EOF
 done
 
