4cc59d66612cacd2955c7bb9aa0e25b79eff4045
chris snow <chsnow123@gmail.com>
2020-04-15 13:36:29 +0100

    fix command name

--- bluedata_infra_main_scripts.tf
+++ bluedata_infra_main_scripts.tf
@@ -68,7 +68,7 @@ resource "local_file" "cli_start_ec2_instances" {
     echo
     echo "           ./bin/terraform_$UPDATE_COMMAND.sh"
     echo 
-    echo "           You should only run ./bin/terraform_apply.sh after all instances are running.  You can check the"
+    echo "           You should only run ./bin/terraform_$UPDATE_COMMAND.sh after all instances are running.  You can check the"
     echo "           instances status with:"
     echo 
     echo "           ./generated/cli_running_ec2_instances.sh"
