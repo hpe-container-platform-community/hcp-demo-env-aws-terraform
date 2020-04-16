c80249ff1b4e997f90338ff6847e4984575785a6
chris snow <chsnow123@gmail.com>
2020-04-16 07:08:02 +0100

    create initial iam policy for start/stopping instances

---
+++ bluedata_infra_main_iam.tf
@@ -0,0 +1,81 @@
+resource "aws_iam_user" "start_stop_ec2_instances" {
+  name = "${var.project_id}-iam-user"
+
+  tags = {
+    Name = "${var.project_id}-iam-user"
+    Project = "${var.project_id}"
+    user = "${var.user}"
+  }
+}
+
+resource "aws_iam_access_key" "start_stop_ec2_instances_access_key" {
+  user = aws_iam_user.start_stop_ec2_instances.name
+}
+
+data "aws_caller_identity" "current" {}
+
+output "iam_access_key" {
+  value = aws_iam_access_key.start_stop_ec2_instances_access_key.id
+}
+
+output "iam_secret" {
+  value =  aws_iam_access_key.start_stop_ec2_instances_access_key.secret
+}
+
+resource "aws_iam_user_policy" "start_stop_ec2_instances" {
+  name = "${var.project_id}-start-stop-ec2-instances"
+  user = aws_iam_user.start_stop_ec2_instances.name
+
+  policy = <<-EOF
+  {
+      "Version": "2012-10-17",
+      "Statement": [
+          {
+              "Sid": "StartStopIfTags",
+              "Effect": "Allow",
+              "Action": [
+                  "ec2:StartInstances",
+                  "ec2:StopInstances",
+                  "ec2:DescribeTags"
+              ],
+              "Resource": "arn:aws:ec2:${var.region}:${data.aws_caller_identity.current.account_id}:instance/*",
+              "Condition": {
+                  "StringEquals": {
+                      "ec2:ResourceTag/Project": "${var.project_id}",
+                      "ec2:ResourceTag/user": "${var.user}"
+                  }
+              }
+          }
+      ]
+  }
+  EOF
+}
+
+/*
+resource "aws_iam_user_policy" "allow_from_my_ip" {
+  name = "${var.project_id}-allow-from-my-ip"
+  user = aws_iam_user.start_stop_ec2_instances.name
+
+  policy = <<-EOF
+  {
+      "Version": "2012-10-17",
+      "Statement": [
+          {
+              "Sid": "ReplaceNetworkAclEntry",
+              "Effect": "Allow",
+              "Action": [
+                  "ec2:ReplaceNetworkAclEntry"
+              ],
+              "Resource": "arn:aws:ec2:${var.region}:${data.aws_caller_identity.current.account_id}:vpc/vpc-vpc-0e48b0dc596f0eaa4",
+              "Condition": {
+                  "StringEquals": {
+                      "ec2:ResourceTag/Project": "${var.project_id}",
+                      "ec2:ResourceTag/user": "${var.user}"
+                  }
+              }
+          }
+      ]
+  }
+  EOF
+}
+*/
