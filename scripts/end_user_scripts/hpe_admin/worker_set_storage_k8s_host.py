b474cd447545ebe0346d43f2f858611a1892c34c
Chris Snow <chsnow123@gmail.com>
2020-05-26 17:43:47 +0100

    update script

--- scripts/end_user_scripts/hpe_admin/worker_set_storage_k8s_host.py
+++ scripts/end_user_scripts/hpe_admin/worker_set_storage_k8s_host.py
@@ -20,29 +20,30 @@ except:
     sys.exit(1)
 
 controller_public_ip  = j["controller_public_ip"]["value"]  
-ad_server_private_ip  = j["ad_server_private_ip"]["value"]
+install_with_ssl      = j["install_with_ssl"]["value"]  
 
 
 client = ContainerPlatformClient(username='admin', 
                                 password='admin123', 
                                 api_host=controller_public_ip, 
                                 api_port=8080,
-                                use_ssl=True,
-                                verify_ssl=False)
+                                use_ssl=install_with_ssl,
+                                verify_ssl=False,
+				warn_ssl=False)
 client.create_session()
 
 worker_ids = []
-for worker in client.k8s_worker.get_k8shosts():
+for worker in client.k8s_worker.list():
     worker_id = worker.worker_id
     print("Found worker {} with state {}".format(worker_id, worker.status))
-    client.k8s_worker.wait_for_status(worker_id=worker_id, timeout_secs=1200, status=[ WorkerK8sStatus.storage_pending ])
+    #client.k8s_worker.wait_for_status(worker_id=worker_id, timeout_secs=1200, status=[ WorkerK8sStatus.storage_pending ])
     data = {"op_spec": {"persistent_disks": ["/dev/nvme2n1"], "ephemeral_disks": ["/dev/nvme1n1"]}, "op": "storage"}
     client.k8s_worker.set_storage(worker_id=worker_id, data=data)
     worker_ids.append(worker_id)
 
 # wait 20 minutes
-for worker in worker_ids:
-    print("Waiting 20 mins for worker id: {} to have status of ready".format(worker_id))
-    client.k8s_worker.wait_for_status(worker_id=worker_id, timeout_secs=1200, status=[ WorkerK8sStatus.ready ])
+#for worker in worker_ids:
+    #print("Waiting 20 mins for worker id: {} to have status of ready".format(worker_id))
+    #client.k8s_worker.wait_for_status(worker_id=worker_id, timeout_secs=1200, status=[ WorkerK8sStatus.ready ])
 
-print("Worker hosts are ready")
+#print("Worker hosts are ready")
