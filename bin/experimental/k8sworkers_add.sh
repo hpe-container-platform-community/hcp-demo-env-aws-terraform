281aaef55ff78cdef88a9f5f65c6829aecdc6618
Chris Snow <chsnow123@gmail.com>
2020-05-27 10:19:23 +0100

    fix paths

--- bin/experimental/k8sworkers_add.sh
+++ bin/experimental/k8sworkers_add.sh
@@ -3,19 +3,19 @@
 set -e # abort on error
 set -u # abort on undefined variable
 
-./scripts/check_prerequisites.sh
-source ./scripts/variables.sh
+../../scripts/check_prerequisites.sh
+source ../../scripts/variables.sh
 
 pip3 install --quiet --upgrade git+https://github.com/hpe-container-platform-community/hpecp-client@master
 
 # use the project's HPECP CLI config file
-export HPECP_CONFIG_FILE="./generated/hpecp.conf"
+export HPECP_CONFIG_FILE="../../generated/hpecp.conf"
 
 echo "Adding workers"
 WRKR_IDS=()
 for WRKR in ${WRKR_PRV_IPS[@]}; do
     echo "   worker $WRKR"
-    WRKR_IDS+=($(hpecp k8sworker create-with-ssh-key --ip ${WRKR} --ssh-key-file generated/controller.prv_key))
+    WRKR_IDS+=($(hpecp k8sworker create-with-ssh-key --ip ${WRKR} --ssh-key-file ../../generated/controller.prv_key))
 done
 
 # TODO fix: k8sworker wait-for-state
