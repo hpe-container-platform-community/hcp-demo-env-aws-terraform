12bdce8c2c33ee19e1e934a7c2062d000e7ac98b
Chris Snow <chsnow123@gmail.com>
2020-06-02 01:42:16 +0100

    add nics to bigip

--- docs/README-F5-BIGIP/bluedata_infra_main_bigip.tf
+++ docs/README-F5-BIGIP/bluedata_infra_main_bigip.tf
@@ -1,19 +1,68 @@
-resource "aws_instance" "bigip" {
-  # Find AMI for regions outside of Oregaon, here: 
-  # https://aws.amazon.com/marketplace/server/configuration?productId=8e1217d4-a046-4cdf-894e-e38175bae37f&ref_=psb_cfg_continue 
-  ami                    = "ami-0b6bb8289ee2d28d0"
-  instance_type          = "m4.large"
-
-  key_name               = aws_key_pair.main.key_name
-  vpc_security_group_ids = [
+resource "aws_network_interface" "bigip_nic_1" {
+  subnet_id       = module.network.subnet_main_id
+  security_groups = [
     module.network.security_group_allow_all_from_client_ip,
     module.network.security_group_main_id
   ]
-  subnet_id              = module.network.subnet_main_id
+}
+
+resource "aws_network_interface" "bigip_nic_2" {
+  subnet_id       = module.network.subnet_main_id
+  security_groups = [
+    module.network.security_group_allow_all_from_client_ip,
+    module.network.security_group_main_id
+  ]
+}
+
+resource "aws_network_interface" "bigip_nic_3" {
+  subnet_id       = module.network.subnet_main_id
+  security_groups = [
+    module.network.security_group_allow_all_from_client_ip,
+    module.network.security_group_main_id
+  ]
+}
+
+resource "aws_eip" "bigip_eip" {
+  vpc = true
+  tags = {
+    Name = "${var.project_id}-eip-bigip"
+    Project = "${var.project_id}"
+    user = "${var.user}"
+  }
+}
+
+resource "aws_eip_association" "eip_assoc_bigip" {
+  allocation_id = aws_eip.bigip_eip.id
+  network_interface_id = aws_network_interface.bigip_nic_1.id
+}
+
+resource "aws_instance" "bigip" {
+
+  # Find AMI for regions outside of Oregaon, here: 
+  # https://aws.amazon.com/marketplace/server/configuration?productId=8e1217d4-a046-4cdf-894e-e38175bae37f&ref_=psb_cfg_continue 
+
+  ami                    = "ami-0b6bb8289ee2d28d0"
+  instance_type          = "m4.xlarge"
+  availability_zone      = var.az
+
+  key_name               = aws_key_pair.main.key_name
 
   root_block_device {
     volume_type = "gp2"
     volume_size = 400
+  }
+
+  network_interface {
+     network_interface_id = aws_network_interface.bigip_nic_1.id
+     device_index = 0
+  }
+  network_interface {
+     network_interface_id = aws_network_interface.bigip_nic_2.id
+     device_index = 1
+  }
+  network_interface {
+     network_interface_id = aws_network_interface.bigip_nic_3.id
+     device_index = 2
   }
 
   tags = {
@@ -49,9 +98,15 @@ resource "aws_volume_attachment" "bigip-volume-attachment-sdb" {
 }
 
 output "bigip_public_ip" {
-  value = "${aws_instance.bigip.public_ip}"
+  value = "${aws_eip.bigip_eip.public_ip}"
 }
 
-output "bigip_private_ip" {
-  value = "${aws_instance.bigip.private_ip}"
+output "bigip_private_ip_1" {
+  value = "${aws_network_interface.bigip_nic_1.private_ip}"
+}
+output "bigip_private_ip_2" {
+  value = "${aws_network_interface.bigip_nic_2.private_ip}"
+}
+output "bigip_private_ip_3" {
+  value = "${aws_network_interface.bigip_nic_3.private_ip}"
 }
