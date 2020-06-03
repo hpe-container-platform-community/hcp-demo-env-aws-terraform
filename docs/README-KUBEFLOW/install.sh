5ce50b11f41714a954a110aba845875ceb63818e
Chris Snow <chsnow123@gmail.com>
2020-06-03 12:21:22 +0100

    Update install.sh

--- docs/README-KUBEFLOW/install.sh
+++ docs/README-KUBEFLOW/install.sh
@@ -13,8 +13,8 @@ hpecp k8sworker list
 # |    4      | ready  | ip-10-1-0-93.us-west-2.compute.internal  | 10.1.0.93  | /api/v2/worker/k8shost/4  |
 # +-----------+--------+------------------------------------------+------------+---------------------------+
 
-# get the HPE CP supported k8s 1.18.x version number
-KVERS=$(hpecp k8scluster k8s-supported-versions --output text --major-filter 1 --minor-filter 18)
+# get the HPE CP supported k8s 1.17.x version number
+KVERS=$(hpecp k8scluster k8s-supported-versions --output text --major-filter 1 --minor-filter 17)
 echo $KVERS
 
 # setup values from `hpecp k8sworker list'
