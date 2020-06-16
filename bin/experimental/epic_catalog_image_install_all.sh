b800e468e46969b45fb84598b41a7c310f58ddf4
Chris Snow <chsnow123@gmail.com>
2020-06-16 21:05:34 +0100

    improve scripts

--- bin/experimental/epic_catalog_image_install_all.sh
+++ bin/experimental/epic_catalog_image_install_all.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-CATALOG_IMAGES=$(hpecp catalog list --query "[*].[_links.self.href] | []" | tr -d '"[],')
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+CATALOG_IMAGES=$(hpecp catalog list --query "[?state!='installed' && state!='installing' && state!='downloading'] | [*].[_links.self.href]" | tr -d '"[],')
 for IMG_ID in $CATALOG_IMAGES
 do
+  echo "Installing: $IMG_ID"
   hpecp catalog install $IMG_ID
 done
-hpecp catalog list --query "[*].[_links.self.href,label.name,state]"
+
+. ${SCRIPT_DIR}/epic_catalog_image_status.sh
--- bin/experimental/epic_catalog_image_status.sh
+++ bin/experimental/epic_catalog_image_status.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-hpecp catalog list --query "[*].[_links.self.href,label.name,state]"
+source ./scripts/functions.sh
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+echo
+echo -e "${RED}EPIC Catalog Images : Installed${NC}"
+hpecp catalog list --query "[?state=='installed'] | [*].[_links.self.href,label.name,state]" | perl -pe 's/\], \[/,\n/g'
+
+echo
+echo -e "${RED}EPIC Catalog Images : NOT Installed${NC}"
+hpecp catalog list --query "[?state!='installed'] | [*].[_links.self.href,label.name,state]" | perl -pe 's/\], \[/,\n/g'
