5846b95e60b51a46f5756f464774f86a5afc97e4
Chris Snow <chsnow123@gmail.com>
2020-06-16 15:42:55 +0100

    Update epic_catalog_image_install_all.sh

--- bin/experimental/epic_catalog_image_install_all.sh
+++ bin/experimental/epic_catalog_image_install_all.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Installing all catalog images - note that this script does NOT check for status=success."
+
 CATALOG_IMAGES=$(hpecp catalog list --query "[*].[_links.self.href] | []" | tr -d '"[],')
 for IMG_ID in $CATALOG_IMAGES
 do
