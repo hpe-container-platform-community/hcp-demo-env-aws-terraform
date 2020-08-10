5dd89e38c664922c0759b5c7834a0ffdc753ddf2
Chris Snow <chsnow123@gmail.com>
2020-08-10 20:05:24 +0100

    add mapr

---
+++ scripts/mapr_install.sh
@@ -0,0 +1,99 @@
+#!/usr/bin/env bash
+
+set -e # abort on error
+set -u # abort on undefined variable
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+source "$SCRIPT_DIR/variables.sh"
+
+echo PROJECT_DIR="${PROJECT_DIR}"
+echo LOCAL_SSH_PUB_KEY_PATH=${LOCAL_SSH_PUB_KEY_PATH}
+echo LOCAL_SSH_PRV_KEY_PATH=${LOCAL_SSH_PRV_KEY_PATH}
+echo CREATE_EIP_CONTROLLER=${CREATE_EIP_CONTROLLER}
+echo CREATE_EIP_GATEWAY=${CREATE_EIP_GATEWAY}
+echo EPIC_DL_URL=$EPIC_DL_URL
+echo EPIC_FILENAME=$EPIC_FILENAME
+echo EPIC_DL_URL_NEEDS_PRESIGN=$EPIC_DL_URL_NEEDS_PRESIGN
+echo SELINUX_DISABLED=$SELINUX_DISABLED
+echo CTRL_PRV_IP=$CTRL_PRV_IP
+echo CTRL_PUB_IP=$CTRL_PUB_IP
+echo CTRL_PRV_DNS=$CTRL_PRV_DNS
+echo CTRL_PUB_DNS=$CTRL_PUB_DNS
+echo CTRL_PUB_HOST=$CTRL_PUB_HOST
+echo CTRL_PRV_HOST=$CTRL_PRV_HOST
+echo GATW_PRV_IP=$GATW_PRV_IP
+echo GATW_PUB_IP=$GATW_PUB_IP
+echo GATW_PRV_DNS=$GATW_PRV_DNS
+echo GATW_PUB_DNS=$GATW_PUB_DNS
+echo GATW_PUB_HOST=$GATW_PUB_HOST
+echo GATW_PRV_HOST=$GATW_PRV_HOST
+echo WRKR_PRV_IPS=${WRKR_PRV_IPS[@]}
+echo WRKR_PUB_IPS=${WRKR_PUB_IPS[@]}
+echo MAPR_HOSTS_PRV_IPS=${MAPR_HOSTS_PRV_IPS[@]}
+echo MAPR_HOSTS_PUB_IPS=${MAPR_HOSTS_PUB_IPS[@]}
+echo INSTALL_WITH_SSL=${INSTALL_WITH_SSL}
+
+###############################################################################
+# Test SSH connectivity to EC2 instances from local machine
+###############################################################################
+
+ssh -o StrictHostKeyChecking=no -i "${LOCAL_SSH_PRV_KEY_PATH}" -T centos@${CTRL_PUB_IP} 'echo CONTROLLER: $(hostname)'
+ssh -o StrictHostKeyChecking=no -i "${LOCAL_SSH_PRV_KEY_PATH}" -T centos@${GATW_PUB_IP} 'echo GATEWAY: $(hostname)'
+
+for MAPR_HOST in ${MAPR_HOSTS_PUB_IPS[@]}; do 
+   ssh -o StrictHostKeyChecking=no -i "${LOCAL_SSH_PRV_KEY_PATH}" -T centos@${MAPR_HOST} 'echo MAPR_HOST: $(hostname)'
+done
+
+###############################################################################
+# Setup SSH keys for passwordless SSH
+###############################################################################
+
+# We have password SSH access from our local machines to EC2, so we can utiise this to copy the Controller SSH key to each Worker
+for MAPR_HOST in ${MAPR_HOSTS_PUB_IPS[@]}; do 
+    ssh -o StrictHostKeyChecking=no -i "${LOCAL_SSH_PRV_KEY_PATH}" -T centos@${CTRL_PUB_IP} "cat /home/centos/.ssh/id_rsa.pub" | \
+        ssh -o StrictHostKeyChecking=no -i "${LOCAL_SSH_PRV_KEY_PATH}" -T centos@${MAPR_HOST} "cat >> /home/centos/.ssh/authorized_keys"
+done
+
+# test passwordless SSH connection from Controller to Workers
+for MAPR_HOST in ${MAPR_HOSTS_PRV_IPS[@]}; do 
+    ssh -o StrictHostKeyChecking=no -i "${LOCAL_SSH_PRV_KEY_PATH}" -T centos@${CTRL_PUB_IP} << ENDSSH
+        echo CONTROLLER: Connecting to MAPR_HOST ${MAPR_HOST}...
+        ssh -o ConnectTimeout=10 -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa -T centos@${MAPR_HOST} "echo Connected to ${MAPR_HOST}!"
+ENDSSH
+done
+
+###############################################################################
+# Install MAPR
+###############################################################################
+
+ssh -o StrictHostKeyChecking=no -i "${LOCAL_SSH_PRV_KEY_PATH}" -T centos@${CTRL_PUB_IP} << ENDSSH
+   set -x
+
+   if [[ ! -d ~/mapr-ansible ]]; then
+      git clone https://github.com/mapr-emea/mapr-ansible.git
+   fi
+
+   cp -f mapr-ansible/host_templates/hosts_bash.sh hosts_bash.sh
+   cp -f mapr-ansible/host_templates/hosts_cluster hosts_cluster.xml
+
+   sed -i s/10.0.0.132/${MAPR_HOSTS_PRV_IPS[0]}/g ./hosts_cluster.xml
+   sed -i s/ansible_user=ec2-user/ansible_user=centos/g ./hosts_cluster.xml
+   sed -i '1,/\[mapr-opentsdb\]/!d' ./hosts_cluster.xml
+
+   cp ~/.ssh/id_rsa .
+
+   docker run \
+      -v \$PWD:/app \
+      -w /app \
+      -e ANSIBLE_HOST_KEY_CHECKING=False \
+      lexauw/ansible-alpine:latest \
+      ansible-playbook ./mapr-ansible/site-cluster.yml \
+      -i ./hosts_cluster.xml \
+      -u centos \
+      -become \
+      --key-file ./id_rsa \
+      -k
+ENDSSH
+
+
