e2b4542c77ee5f150fda14c6290649dff92b17c0
Chris Snow <chsnow123@gmail.com>
2020-09-08 10:20:27 +0100

    better  user output

--- scripts/check_prerequisites.sh
+++ scripts/check_prerequisites.sh
@@ -3,6 +3,8 @@
 set -e # abort on error
 set -u # abort on undefined variable
 
+source "./scripts/functions.sh"
+
 command -v python3 >/dev/null 2>&1    || { echo >&2 "I require 'python3' but it's not installed.  Aborting."; exit 1; }
 command -v pip3 >/dev/null 2>&1       || { echo >&2 "I require 'pip3' but it's not installed.  Aborting."; exit 1; }
 
@@ -25,7 +27,18 @@ command -v aws >/dev/null 2>&1  || {
 # Ensure python is able to find packages
 REQUIRED_PATH="$(python3 -m site --user-base)/bin"
 if [[ :$PATH: != *:"$REQUIRED_PATH":* ]] ; then
-    echo "PATH variable does not include required '$REQUIRED_PATH'. Aborting."
+    tput setaf 1
+    print_term_width '='
+    echo "Aborting because PATH variable does not include: $REQUIRED_PATH"
+    print_term_width '='
+    tput sgr0
+    echo
+    echo "TIP: You can set the PATH for the current terminal session by running the following command:"
+    echo
+    echo "   export PATH=\$PATH:$REQUIRED_PATH"
+    echo
+    echo "To make the PATH setting permanent, add the above line to your ~/.bashrc or ~/.bash_profile"
+    print_term_width '-'
     exit 1
 fi
 
