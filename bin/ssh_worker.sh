66dfc67c1604d5c00e070e63e268f5ede259abfd
Chris Snow <chsnow123@gmail.com>
2020-09-15 15:32:06 +0100

    update ssh worker

--- bin/ssh_worker.sh
+++ bin/ssh_worker.sh
@@ -4,15 +4,26 @@ WORKER_ID=$1
 
 source "./scripts/variables.sh"
 
+print_workers() {
+    printf "ID\tPUB IP\t\tPRV IP\n"
+    for i in "${!WRKR_PUB_IPS[@]}"; do
+      printf "%s\t%s\t%s\n" "$i" "${WRKR_PUB_IPS[$i]}" "${WRKR_PRV_IPS[$i]}"
+    done
+}
+
 if [[ -z "${WORKER_ID##*[!0-9]*}" ]];
 then
     echo Usage: $0 ID [ARG1, ARG2, ...]
     echo
-    printf "ID\tIP\n"
-    for i in "${!WRKR_PUB_IPS[@]}"; do
-      printf "%s\t%s\n" "$i" "${WRKR_PUB_IPS[$i]}"
-    done
+    print_workers
     exit 1
+fi
+
+if [[ $WORKER_ID -lt 0 || $WORKER_ID -gt ${#WRKR_PUB_IPS[@]}-1 ]]; 
+then
+  echo "$(tput setaf 1)Invalid worker id: $WORKER_ID$(tput sgr0)"
+  print_workers
+  exit 1
 fi
 
 ssh -o StrictHostKeyChecking=no -i "./generated/controller.prv_key" centos@${WRKR_PUB_IPS[$WORKER_ID]} "${@:2}"
