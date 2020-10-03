4296567851e65cd4e72645562f135ff419fafa3e
Chris Snow <chsnow123@gmail.com>
2020-10-03 15:40:26 +0100

    more user hints

--- scripts/check_prerequisites.sh
+++ scripts/check_prerequisites.sh
@@ -24,6 +24,12 @@ command -v aws >/dev/null 2>&1  || {
     exit 1
 }
 
+if [ -f "${HOME}/.bashrc" ] && [ ! -f "${HOME}/.bash_profile" ]; then
+    profile='~/.bashrc'
+else
+    profile='~/.bash_profile'
+fi
+
 # Ensure python is able to find packages
 REQUIRED_PATH="$(python3 -m site --user-base)/bin"
 if [[ :$PATH: != *:"$REQUIRED_PATH":* ]] ; then
@@ -37,7 +43,10 @@ if [[ :$PATH: != *:"$REQUIRED_PATH":* ]] ; then
     echo
     echo "   export PATH=\$PATH:$REQUIRED_PATH"
     echo
-    echo "To make the PATH setting permanent, add the above line to your ~/.bashrc or ~/.bash_profile"
+    echo "To make the PATH setting permanent, add the above line to your ${profile}, e.g."
+    echo
+    echo "   echo 'export PATH=\$PATH:$REQUIRED_PATH' >> ${profile}"
+    echo
     print_term_width '-'
     exit 1
 fi
