c621b6adcb3151551ef1926b688049b41a82622f
Chris Snow <chsnow123@gmail.com>
2020-10-12 23:16:36 +0100

    update ec2 gpu instances

--- bin/ec2_gpu_instances_types.sh
+++ bin/ec2_gpu_instances_types.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-source "./scripts/variables.sh"
-source "./scripts/functions.sh"
+REGION=$1
+
+if [[ -z "$REGION" ]];
+then
+    echo Usage: $0 AWS-REGION
+    echo
+    echo        Example: 
+    echo        --------
+    echo        $0 eu-west-2
+    exit 1
+fi
+
 
 aws ec2 describe-instance-types --query 'InstanceTypes[?GpuInfo!=null].[InstanceType]' --output text --region "${REGION}"
