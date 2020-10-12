3e271c99c710c28290e63a884a99b794a3676ddd
Chris Snow <chsnow123@gmail.com>
2020-10-12 13:20:32 +0100

    add more config for gpu worker hosts

--- bluedata_infra_main_worker_gpu.tf
+++ bluedata_infra_main_worker_gpu.tf
@@ -1,7 +1,36 @@
 locals {
   gpu_worker_count = 0
   gpu_worker_instance_type = "g4dn.xlarge" # change this for your region
+  gpu_worker_has_disk_for_df = false # create /dev/sdc for data fabric - true or false 
+
+  ## don't change below
+  gpu_worker_instance_type_error = "gpu_worker_instance_type '${local.gpu_worker_instance_type}' is invalid for Region '${var.region}'"
 }
+
+########## ASSERT GPU WORKER INSTANCE TYPE IS VALID FOR AZ ##########
+
+data "aws_ec2_instance_type_offerings" "gpu_worker_instance_types" {
+  filter {
+    name   = "instance-type"
+    values = [ "${local.gpu_worker_instance_type}" ]
+  }
+  filter {
+    name   = "location"
+    values = [ "${var.region}" ]
+  }
+  location_type = "region"
+}
+
+resource "null_resource" "gpu_worker_instance_type_validation" {
+  count = (local.gpu_worker_count > 0 && length(data.aws_ec2_instance_type_offerings.gpu_worker_instance_types.instance_types) == 0) ? 1 : 0
+
+  provisioner "local-exec" {
+    command     = "false"
+    interpreter = [ "bash", "-c", "(tput setaf 1 && echo \"${local.gpu_worker_instance_type_error}\" && tput sgr0 && false)"]
+  }
+}
+
+########## END ASSERTION ##########
 
 resource "aws_instance" "workers_gpu" {
   count                  = local.gpu_worker_count
@@ -20,13 +49,16 @@ resource "aws_instance" "workers_gpu" {
     volume_size = 400
   }
 
-
   tags = {
     Name = "${var.project_id}-instance-worker-gpu-${count.index + 1}"
     Project = "${var.project_id}"
     user = "${var.user}"
     deployment_uuid = "${random_uuid.deployment_uuid.result}"
   }
+
+  depends_on = [
+    null_resource.gpu_worker_instance_type_validation,
+  ]
 }
 
 resource "null_resource" "yum_update_workers_gpu" {
@@ -74,7 +106,7 @@ resource "aws_volume_attachment" "worker-gpu-volume-attachment-sdb" {
 # /dev/sdc
 
 resource "aws_ebs_volume" "worker-gpu-ebs-volumes-sdc" {
-  count             = local.gpu_worker_count
+  count             = local.gpu_worker_has_disk_for_df == true ?   local.gpu_worker_count : 0
   availability_zone = var.az
   size              = 1024
   type              = "gp2"
@@ -88,7 +120,7 @@ resource "aws_ebs_volume" "worker-gpu-ebs-volumes-sdc" {
 }
 
 resource "aws_volume_attachment" "worker-gpu-volume-attachment-sdc" {
-  count       = local.gpu_worker_count
+  count       = local.gpu_worker_has_disk_for_df == true ? local.gpu_worker_count : 0
   device_name = "/dev/sdc"
   volume_id   = aws_ebs_volume.worker-gpu-ebs-volumes-sdc.*.id[count.index]
   instance_id = aws_instance.workers_gpu.*.id[count.index]
