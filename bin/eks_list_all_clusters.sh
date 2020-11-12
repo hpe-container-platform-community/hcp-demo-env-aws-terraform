2c36a8b6fee94b938aa42ffe7ad0d236568d651c
Chris Snow <chsnow123@gmail.com>
2020-11-12 16:19:03 +0000

    tidy up

--- bin/eks_list_all_clusters.sh
+++ bin/eks_list_all_clusters.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+trap ctrl_c INT
+
+function ctrl_c() {
+    echo "Got CTRL-C so quitting..."
+    exit
+}
+
 REGIONS=$(aws ec2 describe-regions --region us-east-1 --query 'Regions[*].[RegionName] | []' --output text)
 
 for REGION in $REGIONS; do
-    echo $REGION
-    echo =======
+    tput smul; printf %s\\n "$REGION"; tput rmul
+    echo
     aws eks --region $REGION list-clusters
+    echo
 done
