698801f784b1625d7594da086982df18f7b1a251
Chris Snow <chsnow123@gmail.com>
2020-12-10 22:37:13 +0000

    fix tf warnings

--- bluedata_infra_outputs.tf
+++ bluedata_infra_outputs.tf
@@ -7,7 +7,7 @@ output "user" {
 }
 
 output "aws_profile" {
-  value = "${var.profile}"
+  value = var.profile
 }
 
 output "aws_region" {
@@ -15,11 +15,11 @@ output "aws_region" {
 }
 
 output "subnet_cidr_block" {
-  value = "${var.subnet_cidr_block}"
+  value = var.subnet_cidr_block
 }
 
 output "vpc_cidr_block" {
-  value = "${var.vpc_cidr_block}"
+  value = var.vpc_cidr_block
 }
 
 output "deployment_uuid" {
@@ -27,11 +27,11 @@ output "deployment_uuid" {
 }
 
 output "selinux_disabled" {
-  value = "${var.selinux_disabled}"
+  value = var.selinux_disabled
 }
 
 output "ssh_pub_key_path" {
-  value = "${var.ssh_pub_key_path}"
+  value = var.ssh_pub_key_path
 }
 
 output "ssh_prv_key_path" {
@@ -39,79 +39,79 @@ output "ssh_prv_key_path" {
 }
 
 output "install_with_ssl" {
-  value = "${var.install_with_ssl}"
+  value = var.install_with_ssl
 }
 
 output "ca_cert" {
-  value = "${var.ca_cert}"
+  value = var.ca_cert
 }
 
 output "ca_key" {
-  value = "${var.ca_key}"
+  value = var.ca_key
 }
 
 output "epic_dl_url" {
-  value = "${var.epic_dl_url}"
+  value = var.epic_dl_url
 }
 
 output "epid_dl_url_needs_presign" {
-  value = "${var.epid_dl_url_needs_presign}"
+  value = var.epid_dl_url_needs_presign
 }
 
 output "epic_dl_url_presign_options" {
-  value = "${var.epic_dl_url_presign_options}"
+  value = var.epic_dl_url_presign_options
 }
 
 output "epic_options" {
-  value = "${var.epic_options}"
+  value = var.epic_options
 }
 
 output "client_cidr_block" {
- value = "${var.client_cidr_block}"
+ value = var.client_cidr_block
 }
 
 output "create_eip_controller" {
-  value = "${var.create_eip_controller}"
+  value = var.create_eip_controller
 }
 
 output "create_eip_gateway" {
-  value = "${var.create_eip_gateway}"
+  value = var.create_eip_gateway
 }
 
 output "create_eip_rdp_linux_server" {
-  value = "${var.create_eip_rdp_linux_server}"
+  value = var.create_eip_rdp_linux_server
 }
 
 output "create_eks_cluster" {
-  value = "${var.create_eks_cluster}"
+  value = var.create_eks_cluster
 }
 
 //// Gateway
 
 output "gateway_instance_id" {
-  value = "${module.gateway.id}"
+  value = module.gateway.id
 }
 output "gateway_private_ip" {
-  value = "${module.gateway.private_ip}"
+  value = module.gateway.private_ip
 }
 output "gateway_private_dns" {
-  value = "${module.gateway.private_dns}"
+  value = module.gateway.private_dns
 }
 output "gateway_public_ip" {
-  value = "${module.gateway.public_ip}"
+  value = module.gateway.public_ip
 }
 output "gateway_public_dns" {
-  value = "${module.gateway.public_dns}"
+  value = module.gateway.public_dns
 }
 
 //// Controllers
 
 output "controller_instance_id" {
-  value = "${module.controller.id}"
+  value = module.controller.id
 }
 
 output "controller_public_ip" {
-  value = "${module.controller.public_ip}"
+  value = module.controller.public_ip
 }
 
 output "controller_public_url" {
@@ -119,113 +119,113 @@ output "controller_public_url" {
 }
 
 output "controller_private_ip" {
-  value = "${module.controller.private_ip}"
+  value = module.controller.private_ip
 }
 
 output "controller_public_dns" {
-  value = "${module.controller.public_dns}"
+  value = module.controller.public_dns
 }
 
 output "controller_private_dns" {
-  value = "${module.controller.private_dns}"
+  value = module.controller.private_dns
 }
 
 /// workers
 
 output "workers_instance_id" {
-  value = ["${aws_instance.workers.*.id}"]
+  value = [aws_instance.workers.*.id]
 }
 output "workers_public_ip" {
-  value = ["${aws_instance.workers.*.public_ip}"]
+  value = [aws_instance.workers.*.public_ip]
 }
 output "workers_public_dns" {
-  value = ["${aws_instance.workers.*.public_dns}"]
+  value = [aws_instance.workers.*.public_dns]
 }
 output "workers_private_ip" {
-  value = ["${aws_instance.workers.*.private_ip}"]
+  value = [aws_instance.workers.*.private_ip]
 }
 output "workers_private_dns" {
-  value = ["${aws_instance.workers.*.private_dns}"]
+  value = [aws_instance.workers.*.private_dns]
 }
 
 output "worker_count" {
-  value = ["${var.worker_count}"]
+  value = [var.worker_count]
 }
 
 /// GPU workers
 
 output "workers_gpu_instance_id" {
-  value = ["${aws_instance.workers_gpu.*.id}"]
+  value = [aws_instance.workers_gpu.*.id]
 }
 output "workers_gpu_public_ip" {
-  value = ["${aws_instance.workers_gpu.*.public_ip}"]
+  value = [aws_instance.workers_gpu.*.public_ip]
 }
 output "workers_gpu_public_dns" {
-  value = ["${aws_instance.workers_gpu.*.public_dns}"]
+  value = [aws_instance.workers_gpu.*.public_dns]
 }
 output "workers_gpu_private_ip" {
-  value = ["${aws_instance.workers_gpu.*.private_ip}"]
+  value = [aws_instance.workers_gpu.*.private_ip]
 }
 output "workers_gpu_private_dns" {
-  value = ["${aws_instance.workers_gpu.*.private_dns}"]
+  value = [aws_instance.workers_gpu.*.private_dns]
 }
 
 output "gpu_worker_count" {
-  value = ["${var.gpu_worker_count}"]
+  value = [var.gpu_worker_count]
 }
 
 //// MAPR Cluster 1
 
 output "mapr_cluster_1_hosts_instance_id" {
-  value = ["${aws_instance.mapr_cluster_1_hosts.*.id}"]
+  value = [aws_instance.mapr_cluster_1_hosts.*.id]
 }
 output "mapr_cluster_1_hosts_public_ip" {
-  value = ["${aws_instance.mapr_cluster_1_hosts.*.public_ip}"]
+  value = [aws_instance.mapr_cluster_1_hosts.*.public_ip]
 }
 output "mapr_cluster_1_hosts_public_dns" {
-  value = ["${aws_instance.mapr_cluster_1_hosts.*.public_dns}"]
+  value = [aws_instance.mapr_cluster_1_hosts.*.public_dns]
 }
 output "mapr_cluster_1_hosts_private_ip" {
-  value = ["${aws_instance.mapr_cluster_1_hosts.*.private_ip}"]
+  value = [aws_instance.mapr_cluster_1_hosts.*.private_ip]
 }
 output "mapr_cluster_1_hosts_private_ip_flat" {
   value = join("\n", aws_instance.mapr_cluster_1_hosts.*.private_ip)
 }
 output "mapr_cluster_1_hosts_private_dns" {
-  value = ["${aws_instance.mapr_cluster_1_hosts.*.private_dns}"]
+  value = [aws_instance.mapr_cluster_1_hosts.*.private_dns]
 }
 output "mapr_cluster_1_count" {
-  value = ["${var.mapr_cluster_1_count}"]
+  value = [var.mapr_cluster_1_count]
 }
 output "mapr_cluster_1_name" {
-  value = ["${var.mapr_cluster_1_name}"]
+  value = [var.mapr_cluster_1_name]
 }
 
 /// MAPR Cluster 2
 
 output "mapr_cluster_2_hosts_instance_id" {
-  value = ["${aws_instance.mapr_cluster_2_hosts.*.id}"]
+  value = [aws_instance.mapr_cluster_2_hosts.*.id]
 }
 output "mapr_cluster_2_hosts_public_ip" {
-  value = ["${aws_instance.mapr_cluster_2_hosts.*.public_ip}"]
+  value = [aws_instance.mapr_cluster_2_hosts.*.public_ip]
 }
 output "mapr_cluster_2_hosts_public_dns" {
-  value = ["${aws_instance.mapr_cluster_2_hosts.*.public_dns}"]
+  value = [aws_instance.mapr_cluster_2_hosts.*.public_dns]
 }
 output "mapr_cluster_2_hosts_private_ip" {
-  value = ["${aws_instance.mapr_cluster_2_hosts.*.private_ip}"]
+  value = [aws_instance.mapr_cluster_2_hosts.*.private_ip]
 }
 output "mapr_cluster_2_hosts_private_ip_flat" {
   value = join("\n", aws_instance.mapr_cluster_2_hosts.*.private_ip)
 }
 output "mapr_cluster_2_hosts_private_dns" {
-  value = ["${aws_instance.mapr_cluster_2_hosts.*.private_dns}"]
+  value = [aws_instance.mapr_cluster_2_hosts.*.private_dns]
 }
 output "mapr_cluster_2_count" {
-  value = ["${var.mapr_cluster_2_count}"]
+  value = [var.mapr_cluster_2_count]
 }
 output "mapr_cluster_2_name" {
-  value = ["${var.mapr_cluster_2_name}"]
+  value = [var.mapr_cluster_2_name]
 }
 
 
