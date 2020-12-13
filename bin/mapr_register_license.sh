ddb16c40a0ca57e5ec77751135ca5843aa46fab6
Chris Snow <chsnow123@gmail.com>
2020-12-13 01:13:06 +0000

    add license for cluster 2

--- bin/mapr_register_license.sh
+++ bin/mapr_register_license.sh
@@ -34,8 +34,6 @@ if [[ "$MAPR_CLUSTER1_COUNT" != "0" ]]; then
       URL+="&maprcom_username=\$(echo $username | perl -ne 'chomp and print' | base64)"
       URL+="&maprcom_password=\$(echo $password | perl -ne 'chomp and print' | base64)"
 
-      echo \$URL
-
       curl -s \$URL \
          | jq ".data.cluster.licenses[0].key_contents" \
          | sed 's!^"!!g' \
@@ -46,11 +44,50 @@ if [[ "$MAPR_CLUSTER1_COUNT" != "0" ]]; then
          > license.txt
 
       maprcli license add -cluster $MAPR_CLUSTER1_NAME -license license.txt -is_file true
+      RESPONSE=$?
+
+      if [[ \$RESPONSE == 0 || \$RESPONSE == 17 ]]; then
+         exit 0
+      else
+         echo "Error adding license"
+         exit 1
+      fi
 EOF
 fi
 
 if [[ "$MAPR_CLUSTER2_COUNT" != "0" ]]; then
-   echo 123
+
+   ./generated/ssh_mapr_cluster_2_host_0.sh sudo apt-get install -y jq
+
+   ./generated/ssh_mapr_cluster_2_host_0.sh <<EOF
+
+      CLUSTERID=\$(maprcli license showid -cluster $MAPR_CLUSTER2_NAME | tail -1 | tr -d ' ')
+
+      URL="https://mapr-installer-dialhome.appspot.com/trial?"
+      URL+="cluster_name=$MAPR_CLUSTER2_NAME"
+      URL+="&cluster_id=\$CLUSTERID"
+      URL+="&maprcom_username=\$(echo $username | perl -ne 'chomp and print' | base64)"
+      URL+="&maprcom_password=\$(echo $password | perl -ne 'chomp and print' | base64)"
+
+      curl -s \$URL \
+         | jq ".data.cluster.licenses[0].key_contents" \
+         | sed 's!^"!!g' \
+         | sed 's!"\$!!g' \
+         | sed 's!\\\\"!"!g' \
+         | perl -pe 's/\\\\n/\\n/g' \
+         | perl -pe 's/\\\\r/\\r/g' \
+         > license.txt
+
+      maprcli license add -cluster $MAPR_CLUSTER2_NAME -license license.txt -is_file true
+      RESPONSE=$?
+
+      if [[ \$RESPONSE == 0 || \$RESPONSE == 17 ]]; then
+         exit 0
+      else
+         echo "Error adding license"
+         exit 1
+      fi
+EOF
 fi
 
 
