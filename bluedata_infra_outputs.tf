2baf6f1bfdc388c1ea5b8fd0b62bbff408e42c66
Chris Snow <chsnow123@gmail.com>
2020-12-14 12:03:31 +0000

    output terraform mapr hosts

--- bluedata_infra_outputs.tf
+++ bluedata_infra_outputs.tf
@@ -191,6 +191,9 @@ output "mapr_cluster_1_hosts_private_ip" {
 output "mapr_cluster_1_hosts_private_ip_flat" {
   value = join("\n", aws_instance.mapr_cluster_1_hosts.*.private_ip)
 }
+output "mapr_cluster_1_hosts_public_ip_flat" {
+  value = join("\n", aws_instance.mapr_cluster_1_hosts.*.public_ip)
+}
 output "mapr_cluster_1_hosts_private_dns" {
   value = [aws_instance.mapr_cluster_1_hosts.*.private_dns]
 }
@@ -217,6 +220,9 @@ output "mapr_cluster_2_hosts_private_ip" {
 }
 output "mapr_cluster_2_hosts_private_ip_flat" {
   value = join("\n", aws_instance.mapr_cluster_2_hosts.*.private_ip)
+}
+output "mapr_cluster_2_hosts_public_ip_flat" {
+  value = join("\n", aws_instance.mapr_cluster_2_hosts.*.public_ip)
 }
 output "mapr_cluster_2_hosts_private_dns" {
   value = [aws_instance.mapr_cluster_2_hosts.*.private_dns]
