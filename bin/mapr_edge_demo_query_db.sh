b2e136001e45fa76f30237483d4f1c60055f7f0b
Chris Snow <chsnow123@gmail.com>
2020-12-15 09:08:03 +0000

    add dbshell query

---
+++ bin/mapr_edge_demo_query_db.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export HIDE_WARNINGS=1
+
+source "./scripts/variables.sh"
+source "./scripts/functions.sh"
+
+################################################################################
+print_header "dbshell docs - https://docs.datafabric.hpe.com/62/ReferenceGuide/mapr_dbshell.html"
+################################################################################
+
+./generated/ssh_mapr_cluster_1_host_0.sh sudo -u mapr bash <<EOF
+   exec 2> /dev/null
+   mapr dbshell --cmdfile <(echo 'find /apps/pipeline/data/imagesTable --limit 1 --pretty') 
+EOF
