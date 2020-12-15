cbf77def93b5d6729a3bba52ad0bb7386ba3f071
Chris Snow <chsnow123@gmail.com>
2020-12-15 09:13:14 +0000

    more info in output

--- bin/mapr_edge_demo_query_db.sh
+++ bin/mapr_edge_demo_query_db.sh
@@ -7,6 +7,9 @@ source "./scripts/functions.sh"
 
 ################################################################################
 print_header "dbshell docs - https://docs.datafabric.hpe.com/62/ReferenceGuide/mapr_dbshell.html"
+echo
+echo "Running 'find /apps/pipeline/data/imagesTable --limit 1 --pretty'"
+print_term_width '-'
 ################################################################################
 
 ./generated/ssh_mapr_cluster_1_host_0.sh sudo -u mapr bash <<EOF
