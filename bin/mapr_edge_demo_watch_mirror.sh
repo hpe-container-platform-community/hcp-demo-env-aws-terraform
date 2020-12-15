7973e6c77ebc7ecb22a6feea62319c412e69dedf
Chris Snow <chsnow123@gmail.com>
2020-12-15 08:07:17 +0000

    add mirror watch script

---
+++ bin/mapr_edge_demo_watch_mirror.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+HIDE_WARNINGS=1
+
+source "./scripts/variables.sh"
+
+./generated/ssh_mapr_cluster_2_host_0.sh -t \
+   "bash -c 'watch ls -lr /mapr/dc1.enterprise.org/apps/pipeline/data/files-missionX /mapr/edge1.enterprise.org/apps/pipeline/data/files-missionX'"
+
