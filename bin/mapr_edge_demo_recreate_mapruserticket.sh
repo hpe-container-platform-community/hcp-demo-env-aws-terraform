426d8116fab600f56c3df1ab0cd6903fd98b71ab
Chris Snow <chsnow123@gmail.com>
2020-12-18 09:29:30 +0000

    script to create mapuserticket

---
+++ bin/mapr_edge_demo_recreate_mapruserticket.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+HIDE_WARNINGS=1
+
+source "./scripts/variables.sh"
+
+DC_MAPR_USERTICKET="$(HIDE_WARNINGS=1 ./generated/ssh_mapr_cluster_1_host_0.sh 'sudo head -n1 /opt/mapr/conf/mapruserticket')"
+EDGE_MAPR_USERTICKET="$(HIDE_WARNINGS=1 ./generated/ssh_mapr_cluster_2_host_0.sh 'sudo head -n1 /opt/mapr/conf/mapruserticket')"
+
+for I in 0 1 2; do
+   echo "$DC_MAPR_USERTICKET" | \
+      ./generated/ssh_mapr_cluster_1_host_$I.sh "sudo bash -c 'cat > /opt/mapr/conf/mapruserticket'"
+
+   echo "$EDGE_MAPR_USERTICKET" | \
+      ./generated/ssh_mapr_cluster_2_host_$I.sh "sudo bash -c 'cat > /opt/mapr/conf/mapruserticket'"
+done;
+
+for I in 0 1 2; do
+   echo "$DC_MAPR_USERTICKET" | \
+      ./generated/ssh_mapr_cluster_2_host_$I.sh "sudo bash -c 'cat >> /opt/mapr/conf/mapruserticket'"
+      
+   echo "$EDGE_MAPR_USERTICKET" | \
+      ./generated/ssh_mapr_cluster_1_host_$I.sh "sudo bash -c 'cat >> /opt/mapr/conf/mapruserticket'"
+done;
+
+for i in 1 2; do   
+  for j in 0 1 2; do    
+    echo CLUSTER $i HOST $j;   
+    ./generated/ssh_mapr_cluster_${i}_host_${j}.sh "sudo cat /opt/mapr/conf/mapruserticket"
+  done;
+done;
