84c5bcf08463ea73972a145c5b17d6c139dca7ca
Chris Snow <chsnow123@gmail.com>
2020-12-18 09:40:17 +0000

    add login to script

--- bin/mapr_edge_demo_poststartup_edge_replica.sh
+++ bin/mapr_edge_demo_poststartup_edge_replica.sh
@@ -4,5 +4,14 @@ export HIDE_WARNINGS=1
 
 source "./scripts/variables.sh"
 
-./generated/ssh_mapr_cluster_1_host_0.sh \
-   "sudo -u mapr bash -c '. /home/mapr/microservices-dashboard/scripts/hq/create-edge-replica.sh'"
+
+./generated/ssh_mapr_cluster_1_host_0.sh sudo -u mapr bash <<EOF
+
+   echo mapr | maprlogin password -user mapr
+   maprlogin authtest
+
+   echo mapr | maprlogin password -user mapr -cluster dc1.enterprise.org
+   maprlogin authtest -cluster dc1.enterprise.org
+
+   . /home/mapr/microservices-dashboard/scripts/hq/create-edge-replica.sh
+EOF
