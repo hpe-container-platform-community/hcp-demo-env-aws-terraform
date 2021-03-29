46acc2bc845a1467aa2cb512f68da12918b0da04
Chris Snow <chsnow123@gmail.com>
2021-03-29 17:53:22 +0100

    support individual params

--- bin/create_new_environment_from_scratch_with_picasso.sh
+++ bin/create_new_environment_from_scratch_with_picasso.sh
@@ -10,12 +10,22 @@ MASTER_HOSTS_INDEX='0:3'
 # select the remaining ECP worker host hosts from terraform as the k8s workers
 WORKER_HOSTS_INDEX='3:'
 
+pip3 install --quiet --upgrade --user hpecp
+
+# use the project's HPECP CLI config file
+export HPECP_CONFIG_FILE="./generated/hpecp.conf"
+
+# Test CLI is able to connect
+echo "Platform ID: $(hpecp license platform-id)"
+
 # verify terraform isn't deploying ECP  with embedded DF
 if grep '^\s*embedded_df\s*=\s*true\s*' etc/bluedata_infra.tfvars;
 then
    echo "'embedded_df' must be set to 'false' in 'etc/bluedata_infra.tfvars'"
    exit 1
 fi
+
+
 
 # start from a clean slate - remove all trace of previous runs
 ./bin/terraform_destroy_accept.sh
@@ -46,13 +56,28 @@ K8S_VERSION=$(hpecp k8scluster k8s-supported-versions --major-filter 1 --minor-f
 
 AD_SERVER_PRIVATE_IP=$(terraform output ad_server_private_ip)
 
-EXTERNAL_IDENTITY_SERVER="{\"bind_pwd\":\"5ambaPwd@\",\"user_attribute\":\"CN\",\"bind_type\":\"search_bind\",\"bind_dn\":\"cn=Administrator,CN=Users,DC=samdom,DC=example,DC=com\",\"host\":\"${AD_SERVER_PRIVATE_IP}\",\"group_attribute\":\"member\",\"security_protocol\":\"ldaps\",\"base_dn\":\"CN=Users,DC=samdom,DC=example,DC=com\",\"verify_peer\":false,\"type\":\"Active Directory\",\"port\":636}"
-
 K8S_HOST_CONFIG="$(echo $MASTER_IDS | sed 's/ /:master,/g'):master,$(echo $WORKER_IDS | sed 's/ /:worker,/g'):worker"
 echo K8S_HOST_CONFIG=$K8S_HOST_CONFIG
 
 echo "Creating k8s cluster with version ${K8S_VERSION}"
-CLUSTER_ID=$(hpecp k8scluster create --name c1 --k8s-version $K8S_VERSION --k8shosts-config "$K8S_HOST_CONFIG" --external-identity-server "${EXTERNAL_IDENTITY_SERVER}" --external-groups '["CN=DemoTenantAdmins,CN=Users,DC=samdom,DC=example,DC=com","CN=DemoTenantUsers,CN=Users,DC=samdom,DC=example,DC=com"]' --datafabric true --datafabric-name=dfdemo)
+CLUSTER_ID=$(hpecp k8scluster create \
+   --name c1 \
+   --k8s-version $K8S_VERSION \
+   --k8shosts-config "$K8S_HOST_CONFIG" \
+   --ext_id_svr_bind_pwd "5ambaPwd@" \
+   --ext_id_svr_user_attribute "CN" \
+   --ext_id_svr_bind_type "search_bind" \
+   --ext_id_svr_bind_dn "cn=Administrator,CN=Users,DC=samdom,DC=example,DC=com" \
+   --ext_id_svr_host "${AD_SERVER_PRIVATE_IP}" \
+   --ext_id_svr_group_attribute "member" \
+   --ext_id_svr_security_protocol "ldaps" \
+   --ext_id_svr_base_dn "CN=Users,DC=samdom,DC=example,DC=com" \
+   --ext_id_svr_verify_peer false \
+   --ext_id_svr_type "Active Directory" \
+   --ext_id_svr_port 636 \
+   --external-groups '["CN=DemoTenantAdmins,CN=Users,DC=samdom,DC=example,DC=com","CN=DemoTenantUsers,CN=Users,DC=samdom,DC=example,DC=com"]' \
+   --datafabric true \
+   --datafabric-name=dfdemo)
 
 echo CONTROLLER URL: $(terraform output controller_public_url)
 
