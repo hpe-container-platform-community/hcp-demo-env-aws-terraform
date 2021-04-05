141cdf4b53ca9c020b09f175a8d7fa0025922497
Chris Snow <chsnow123@gmail.com>
2021-04-05 10:25:51 +0100

    Create get_logs.sh

---
+++ bin/get_logs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source "./scripts/variables.sh"
+
+
+THE_DATE=$(date +"%Y-%m-%dT%H-%M-%S%z")
+   
+./bin/ssh_controller.sh sudo tar czf - /var/log/bluedata/ > ${THE_DATE}-controller-logs.tar.gz
+       
+for i in "${!WRKR_PUB_IPS[@]}"; do
+  ssh -o StrictHostKeyChecking=no -i "./generated/controller.prv_key" centos@${WRKR_PUB_IPS[$i]} sudo tar czf - /var/log/bluedata/ > ${THE_DATE}-worker-${i}-logs.tar.gz
+done
