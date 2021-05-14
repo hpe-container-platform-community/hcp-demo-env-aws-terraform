3794b7898648801b8cf8cfe1cfb1d3001f5d4a84
EC2 Default User <ec2-user@ip-10-1-0-70.eu-west-1.compute.internal>
2021-05-14 18:14:04 +0000

    fix eks script

--- scripts/eks_setup.sh
+++ scripts/eks_setup.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+
 terraform output eks-server-url > generated/eks_server_url
 
-aws eks --region $(terraform output aws_region) update-kubeconfig --name $(terraform output eks-cluster-name) --kubeconfig generated/eks_kubeconfig
+aws eks --region $(terraform output -raw aws_region) update-kubeconfig --name $(terraform output -raw eks-cluster-name) --kubeconfig generated/eks_kubeconfig
+
 
 kubectl --kubeconfig generated/eks_kubeconfig create serviceaccount abc123
 kubectl --kubeconfig generated/eks_kubeconfig create clusterrolebinding add-on-cluster-admin --clusterrole=cluster-admin --serviceaccount=default:abc123
-
 SA_TOKEN=$(kubectl --kubeconfig generated/eks_kubeconfig get serviceaccount/abc123 -o jsonpath={.secrets[0].name})
+
+set -e
+set -u
+set -o pipefail
 
 kubectl --kubeconfig generated/eks_kubeconfig get secret $SA_TOKEN  -o jsonpath={'.data.token'} > generated/eks_token.base64
 kubectl --kubeconfig generated/eks_kubeconfig get secret $SA_TOKEN  -o jsonpath={'.data.ca\.crt'} > generated/eks_ca.crt.base64
